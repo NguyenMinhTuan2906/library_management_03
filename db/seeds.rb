144012f731fd5654d57f40855a4f1d8cf39b5daa
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-04-02 16:10:09 +0700

    finish book order

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,7 @@ end
   name = Faker::Name.name
   name1 = Faker::Name.name
   name2 = Faker::Name.name
+  name3 = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "asdasd"
   User.create! name: name,
@@ -30,4 +31,6 @@ end
   Publisher.create! name: name2,
     description: "Khung Bo Cao Hieu",
     address: "#{rand(0..99)} Ha Noi"
+  Book.create name: name3, paperback: 111, image: "ruby.jpg", author_id: rand(1..20), publisher_id: rand(1..20), category_id: rand(1..6)
 end
+
