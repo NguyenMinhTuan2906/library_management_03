81b22a04201dc8aabac32c10a10df8d90a5b0482
NguyenMinhTuan2906 <monteeladykillah@gmail.com>
2017-04-02 19:17:18 +0700

    Finish following user

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,15 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :books
   resources :authors, only: :show
+  resources :relationships, only: [:create, :destroy]
 
   namespace :admin do
     root "homes#index"
